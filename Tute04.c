4683ddc4e79ac6fac35b2266702bf762aef44bfd
it21022142 <87350637+it21022142@users.noreply.github.com>
2021-07-13 04:11:56 +0000

    Funtions are included

--- Tute04.c
+++ Tute04.c
@@ -5,6 +5,9 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+int minimum(int x,int y);
+int maximum(int x,int y);
+int multiply(int x,int y);
 
 int main() {
    int no1, no2;
@@ -16,4 +19,19 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
+}
+int minimum(int x,int y){
+	if(x < y){
+		return x;
+	}
+	else y;
+}
+int maximum(int x,int y){
+	if(x < y){
+		return y;
+	}
+	else x;
+}
+int multiply(int x,int y){
+	return x*y;
 }
