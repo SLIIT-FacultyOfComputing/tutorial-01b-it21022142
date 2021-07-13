393e1b50b046a0c7092da9cace01d9f9a0c158a9
it21022142 <87350637+it21022142@users.noreply.github.com>
2021-07-13 03:54:37 +0000

    Entered the code

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,17 @@
 
 int main() {
   
+	int m1,m2;
+	float avg;
+	printf("Enter mark 1 : ");
+	scanf("%d",&m1);
+	printf("Enter mark 2 : ");
+	scanf("%d",&m2);
+	
+	avg = (m1 + m2)/(2.0);
+	printf("Average : %f",avg);
+	
+
   return 0;
 }
 
